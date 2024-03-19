30efd3c6a2bcb5929e1ba64f07b9b9cac20fcdd1
BoredGuyy <ktourabi@gmail.com>
2024-03-19 22:57:59 +0000

    full program

--- todo.c
+++ todo.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 typedef enum
 {
@@ -9,11 +11,13 @@ typedef enum
     Haute
 }Priorite;
 
+typedef struct tm DueDate;
+
 typedef struct
 {
     char titre[100];
     Priorite priorite;
-    char date[11];
+    DueDate dueDate;
     int status;
 }Tache;
 
@@ -22,6 +26,7 @@ int tache_Num = 0;
 
 void ajouterTache()
 {
+    system("clear");
     if(tache_Num >= 50)
     {
         printf("Can't add any more tasks, List is full!\n");
@@ -29,32 +34,35 @@ void ajouterTache()
     }
 
     printf("Enter Task title: ");
-    scanf("%s", taches[tache_Num].titre);
+    scanf(" %[^\n]", taches[tache_Num].titre);
+
 
     printf("Enter the priority (1. Low, 2. Medium, 3. High): ");
     scanf("%d", (int*)&taches[tache_Num].priorite);
 
-    printf("Enter Due date: ");
-    scanf("%s", taches[tache_Num].date);
+    printf("Enter Due date (DD/MM/YYYY): ");
+    scanf("%d/%d/%d", &taches[tache_Num].dueDate.tm_mday, &taches[tache_Num].dueDate.tm_mon, &taches[tache_Num].dueDate.tm_year);
 
     taches[tache_Num].status = 0;
 
     tache_Num++;
-
     printf("Task added successfuly!\n");
 }
 
 void afficherTaches()
 {
-    printf("Tasks:\n");
+    system("clear");
+    printf("\n-------------------- Tasks: ----------------------------------\n\n");
     for (int i = 0; i < tache_Num; i++)
     {
-        printf("%d - Task Description: %s - Task Priority: %d - Task Due Date: %s - Task Status: %d\n",i , taches[i].titre, (int)taches[i].priorite, taches[i].date, taches[i].status);
+        printf("%d - Task Description: %s - Task Priority: %s - Task Due Date: %02d/%02d/%d - Task Status: %s\n\n",i , taches[i].titre, ((int)taches[i].priorite == 1) ? "Low" : ((int)taches[i].priorite == 2) ? "Average" : ((int)taches[i].priorite == 3) ? "High" : "Unknown", taches[i].dueDate.tm_mday, taches[i].dueDate.tm_mon, taches[i].dueDate.tm_year, (taches[i].status == 0) ? "Not Done" : "Done");
     }
+    printf("----------------------------------------------------------------\n");
 }
 
 void modifierTache()
 {
+    system("clear");
     afficherTaches();
 
     if (tache_Num == 0)
@@ -74,19 +82,20 @@ void modifierTache()
     }
     
     printf("Enter New Task title: ");
-    scanf("%s", taches[choice].titre);
+    scanf(" %[^\n]", taches[tache_Num].titre);
 
     printf("Enter New priority (1. Low, 2. Medium, 3. High): ");
     scanf("%d", (int*)&taches[choice].priorite);
 
-    printf("Enter New Due date: ");
-    scanf("%s", taches[choice].date);
+    printf("Enter New Due date (DD/MM/YYYY): ");
+    scanf("%d/%d/%d", &taches[choice].dueDate.tm_mday, &taches[choice].dueDate.tm_mon, &taches[choice].dueDate.tm_year);
 
     printf("Task modified successfuly!\n");
 }
 
 void supprimerTache()
 {
+    system("clear");
     if(tache_Num <= 0)
     {
         printf("There are no tasks to delete!\n");
@@ -116,13 +125,14 @@ void supprimerTache()
 
 void filtrage()
 {
+    system("clear");
     if (tache_Num == 0)
     {
         printf("No tasks to filter yet!\n");
         return;
     }
 
-    printf("Filtrage option:\n");
+    printf("\nFiltrage option:\n");
     printf("1- Filter by Priority\n");
     printf("2- Filter by Due Date\n");
     
@@ -131,38 +141,97 @@ void filtrage()
 
     if(choice == 1)
     {
-        int priorite = 3;
-        do
+        system("clear");
+        int priorite;
+        printf("Priorite You want to filter with: (1. Low, 2. Average, 3.High): ");
+        scanf("%d", &priorite);
+
+        printf("\n-------------------- Tasks: ----------------------------------\n\n");
+        for (int i = 0; i < tache_Num; i++)
         {
-            for (int i = 0; i < tache_Num; i++)
-            {
-                if(taches[i].priorite == priorite)
-                    printf("%d - Task Description: %s - Task Priority: %d - Task Due Date: %s - Task Status: %d\n",i , taches[i].titre, (int)taches[i].priorite, taches[i].date, taches[i].status);
-            }
-            priorite--;
-        } while (priorite > 0);
-                
+            if(priorite == taches[i].priorite)
+                printf("%d - Task Description: %s - Task Priority: %s - Task Due Date: %02d/%02d/%d - Task Status: %s\n\n",i , taches[i].titre, ((int)taches[i].priorite == 1) ? "Low" : ((int)taches[i].priorite == 2) ? "Average" : ((int)taches[i].priorite == 3) ? "High" : "Unknown", taches[i].dueDate.tm_mday, taches[i].dueDate.tm_mon, taches[i].dueDate.tm_year, (taches[i].status == 0) ? "Not Done" : "Done");
+        }
+        printf("\n--------------------------------------------------------------\n\n");
+        
     }
     else if(choice == 2)
     {
+        system("clear");
+        typedef struct tm Date;
+        Date date;
+        printf("Enter Date to sort with (DD/MM/YYYY): ");
+        scanf("%d/%d/%d", &date.tm_mday, &date.tm_mon, &date.tm_year);
+
+        printf("\n-------------------- Tasks: ----------------------------------\n\n");
         for(int i = 0; i < tache_Num; i++)
         {
-            printf("Will be done soon!\n");
-            return;
+            if(date.tm_mday == taches[i].dueDate.tm_mday && date.tm_mon == taches[i].dueDate.tm_mon && date.tm_year == taches[i].dueDate.tm_year)
+                printf("%d - Task Description: %s - Task Priority: %s - Task Due Date: %02d/%02d/%d - Task Status: %s\n\n",i , taches[i].titre, ((int)taches[i].priorite == 1) ? "Low" : ((int)taches[i].priorite == 2) ? "Average" : ((int)taches[i].priorite == 3) ? "High" : "Unknown", taches[i].dueDate.tm_mday, taches[i].dueDate.tm_mon, taches[i].dueDate.tm_year, (taches[i].status == 0) ? "Not Done" : "Done");
         }
+        printf("\n--------------------------------------------------------------\n\n");
     }
+}
+
+void marqueTache()
+{
+    system("clear");
+    if(tache_Num <= 0)
+    {
+        printf("There are no tasks yet!\n");
+        return;
+    }
+
+    afficherTaches();
+
+    int choice;
+    printf("Task number to modify: ");
+    scanf("%d", &choice);
+
+    if (choice < 0 || choice > tache_Num) {
+        printf("Erreur! Il n'y a pas de tâche avec le numéro que vous avez saisi.\n");
+        return;
+    }
+
+    taches[choice].status = 1;
+
+    printf("Task successufly marked Done\n");
+}
+
+void start()
+{
+    system("clear");
+    int choice;
+
+    do {
+        printf("------------------ TO DO LIST ------------------\n\n");
+        printf("\nMenu:\n");
+        printf("1. Add Task\n");
+        printf("2. Modify Task\n");
+        printf("3. Delete Task\n");
+        printf("4. Filter Task\n");
+        printf("5. Display Tasks\n");
+        printf("6. Marque a Task as Done\n");
+        printf("7. Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: ajouterTache(); break;
+            case 2: modifierTache(); break;
+            case 3: supprimerTache(); break;
+            case 4: filtrage(); break;
+            case 5: afficherTaches(); break;
+            case 6: marqueTache(); break;
+            case 7: printf("Good Bye!\n"); break;
+            default: printf("Choix invalide.\n");
+        }
+    } while (choice != 7);
 }
 
 int main(int argc, char const *argv[])
 {
-    ajouterTache();
-    ajouterTache();
-    ajouterTache();
-    ajouterTache();
-    ajouterTache();
-
-    filtrage();
-    
+          start();
 
     return 0;
 }
